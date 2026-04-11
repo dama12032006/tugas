1eb659e6d62de8c1418c424a0e1930592f4c16fa
dama12032006 <idokdama@gmail.com>
2026-04-11 21:41:33 +0800

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,13 @@
+umur = int(input("Masukkan umur anda: "))
+
+if umur < 0:
+    print("anda belum lahir")
+elif umur > 60:
+    print("banyakin ibadah")
+elif umur >= 18:
+    print("anda cukup umur")
+else:
+    print("anda belum cukup umur")
+    
+
+print("program selesai")
